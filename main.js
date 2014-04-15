529ff23f394be6a17b5041bd3a771886cbca5dfb
Sean Manhertz <sean.manhertz@gmail.com>
2014-04-15 17:34:40 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).on('ready', function() {
+  
+});
+// Outer Shell (dark gray)
+var outerShell = $('<div class="outer-shell"></div>');
+var innerShell = $('<div class="inner-shell"></div>');
+var leftLabels = $('<div class="left-labels"></div>');
+var clockScreen =$('<div class="clock-screen"></div>');
+var clockIndicator =$('<div class="clock-indicator"></div>');
+var clockText = $('<div class="clock-text"></div>');
+var bottomAMLabel = $('<div class="bottom-am-label"></div>');
+var bottomPMLabel =$('<div class="bottom-pm-label"></div>');
+// Inner Shell (black)
+// Left AM/PM & Auto Labels (white)
+// Clock Screen (dark red)
+// Clock AM/PM indicator (red)
+// Clock Text (red)
+// Bottom AM Label & Frequencies (white)
+// Bottom PM Label & Frequencies (white)
