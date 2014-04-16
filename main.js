4354920a274368c577a898bdc08ac0723ee3818c
Sean Manhertz <sean.manhertz@gmail.com>
2014-04-16 17:37:44 -0600

    Have a basic functioning clock with horrible css

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ $(document).on('ready', function() {
 // Outer Shell (dark gray)
 var outerShell = $('<div class="outer-shell"></div>');
 var innerShell = $('<div class="inner-shell"></div>');
-var leftLabels = $('<div class="left-labels"></div>');
+var leftLabels = $('<div class="left-labels"><p>"AM\nAuto"</p></div>');
 var clockScreen =$('<div class="clock-screen"></div>');
 var clockIndicator =$('<div class="clock-indicator"></div>');
-var clockText = $('<div class="clock-text"></div>');
-var bottomAMLabel = $('<div class="bottom-am-label"></div>');
-var bottomPMLabel =$('<div class="bottom-pm-label"></div>');
+var clockText = $('<div id="clock-text"></div>');
+var bottomAMLabel = $('<div class="bottom-am-label"><p>AM 53 60 70 90</p></div>');
+var bottomPMLabel =$('<div class="bottom-pm-label"><p>PM 88 92 96 102</p></div>');
 // Inner Shell (black)
 // Left AM/PM & Auto Labels (white)
 // Clock Screen (dark red)
@@ -17,3 +17,18 @@ var bottomPMLabel =$('<div class="bottom-pm-label"></div>');
 // Clock Text (red)
 // Bottom AM Label & Frequencies (white)
 // Bottom PM Label & Frequencies (white)
+$('.container').append(outerShell);
+$(outerShell).append(innerShell);
+$(innerShell).append(clockScreen);
+$(clockScreen).append(leftLabels).text("PM\nAuto");
+
+var clockTimer = setInterval(function() {myClock()}, 1000);
+  function myClock() {
+    var localTime = new Date().toLocaleTimeString();
+    document.getElementById("clock-text").innerHTML=localTime;
+  };
+// console.log(clockTimer);
+ // $(leftLabels).append(clockScreen);
+ $(clockScreen).append(clockText);
+ $(innerShell).append(bottomAMLabel);
+ $(innerShell).append(bottomPMLabel);
